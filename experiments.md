53f151ed86f5318747e1cbbd3fcfa98a39eeec0a
Artem Khodakov <artem.khodakov.workbox@gmail.com>
2022-04-02 23:16:14 +0300

    feat: matching results

---
+++ experiments.md
@@ -0,0 +1,38 @@
+# Experiments Notes
+
+## 4/2/2022 - Matching
+
+``` python
+# Source variables
+gtmatrix -> ground-truth V3 matrix: [..., [x, y, z]]
+tgmatrix -> target (user) V3 matrix: [..., [x, y, z]]
+
+# Matching algorithm
+# From: https://github.com/SMRT-AIST/fast_gicp
+gicp = pygicp.FastGICP()
+gicp.set_input_target(gtmatrix)
+gicp.set_input_source(tgmatrix)
+matrix = gicp.align()
+
+Matrix: 
+ [[ 0.37804806 -0.925188   -0.03326851 -0.00805712]
+ [ 0.9175803   0.37922916 -0.11929626 -0.00814763]
+ [ 0.12298786  0.01457319  0.99230117 -0.00360233]
+ [ 0.          0.          0.          1.        ]]
+
+matrix[:3, :3] -> rotation matrix
+matrix[:3, 3] -> translation vector
+
+# Transformation formula
+x, y, z = np.dot(matrix[:3, :3], np.array([x, y, z])) + matrix[:3, 3]
+```
+
+Transformation has been applied to the `facility` model.
+
+Top-down view of a user's (red) prediction and ground-truth (blue) model.
+
+![Source](.github/assets/matching-source-04022022.png)
+
+Matrix transformation output:
+
+![Target](.github/assets/matching-results-04022022.png)
--- python/vertices.py
+++ python/vertices.py
@@ -171,8 +171,7 @@ def plot(
     width: int = 512, height: int = 512,
     monocolor: Tuple = None, origin: np.ndarray = None,
     matrix: np.ndarray = None) -> np.ndarray:
-    """ Plot all `Polyline` entities from the document.
-    """
+    """ Plot all `Polyline` entities from the document."""
     origin: np.ndarray = origin if origin is not None else np.full((height, width, 3), 255, dtype=np.uint8)
     for description in tqdm(structures, desc="Plotting", leave=False):
         layer = description["layer"]
@@ -183,11 +182,16 @@ def plot(
                 color = monocolor or ezdxf.colors.aci2rgb(document.layers.get(layer).color or 0)
 
                 if matrix is not None:
-                    x1, y1, z1 = np.dot(matrix[:3, :3], np.array([x1, y1, z1]).T) + matrix[:3, 3]
-                    x2, y2, z2 = np.dot(matrix[:3, :3], np.array([x2, y2, z2]).T) + matrix[:3, 3]
+                    x1, y1, z1 = np.dot(matrix[:3, :3], np.array([x1, y1, z1])) + matrix[:3, 3]
+                    x2, y2, z2 = np.dot(matrix[:3, :3], np.array([x2, y2, z2])) + matrix[:3, 3]
 
                 x1, y1, z1 = int(x1 * width), int(y1 * height), int(z1 * width)
                 x2, y2, z2 = int(x2 * width), int(y2 * height), int(z2 * width)
+
+                # Vor the view only! Does not affect the model representation
+                margin = -200
+                x1, y1, z1 = x1 - margin, y1 - margin, z1 - margin
+                x2, y2, z2 = x2 - margin, y2 - margin, z2 - margin
                 cv2.line(origin, (x1, y1), (x2, y2), tuple(color), 2)
     return origin
 
@@ -208,9 +212,14 @@ def match(gtdoc: Drawing, tgdoc: Drawing, layerlist: List[str] = list()):
     gicp.set_input_source(tgmatrix)
     matrix = gicp.align()
 
-    origin: np.ndarray = np.full((515, 512, 3), 255, dtype=np.uint8)
+    rotation = matrix[:3, :3]
+    transform = matrix[:3, 3]
+    matrix[:3, 3] = -np.dot(np.mean(tgmatrix, 0), rotation) + transform + np.mean(gtmatrix, 0)
+    print("Matrix: \n", matrix)
+
+    origin: np.ndarray = np.full((515 * 2, 512 * 2, 3), 255, dtype=np.uint8)
     origin = plot(gtstruct, gtdoc, 512, 512, (255, 0, 0), origin)
-    origin = plot(tgstruct, tgdoc, 512, 512, (0, 0, 255), origin, matrix)
+    origin = plot(tgstruct, tgdoc, 512, 512, (0, 0, 255), origin)
 
     cv2.imshow("Preview", origin)
     cv2.waitKey(0)
