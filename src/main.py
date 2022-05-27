23d2ce60c58a2f9670eb33cd02a6e3a7d5fa56e2
artem.khodakov <artem.khodakov.workbox@gmail.com>
2022-05-27 16:55:26 +0300

    feat: scores output update

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from email.policy import default
 import json
 import typer
 import dotenv
@@ -36,7 +37,9 @@ def match(
     target_files = loader.read_source(user_data.resolve())
     logging.info(f"Found {len(target_files.keys())} Target models: {list(target_files.keys())}")
 
-    global_metrics = defaultdict(list)
+    global_metrics_output = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    global_iou_output = defaultdict(list)
+
     for model in target_files.keys():
         gtdata = ground_files.get(model)
         if gtdata is None:
@@ -62,65 +65,55 @@ def match(
 
             floors[floor] = matching.match(gtstructures, tgstructures, output, model, floor)
 
-        data = defaultdict(dict)
-        # TODO: Encapsulate it & refactor
-        iou, precision, recall, f1 = [], [], [], []
-        iou_cls = defaultdict(list)
-        precision_cls = defaultdict(list)
-        recall_cls = defaultdict(list)
-        f1_cls = defaultdict(list)
+        # NOTE: If any more data specific operations occurs use Pandas table.
+        # Do not create any more defaultdicts...
+        metrics_output = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+        iou_output = defaultdict(list)
         for floor in floors.values():
             for classname, ious in floor["ious"]["general"].items():
-                iou_cls[classname].append(ious["mean"])
-                iou.append(ious["mean"])
+                mean = ious["mean"]
+                # Collect average IoU over claasses.
+                global_iou_output["average"].append(mean)
+                iou_output["average"].append(mean)
+                # Collect classes specific IoU.
+                global_iou_output[classname].append(mean)
+                iou_output[classname].append(mean)
             for classname, metrics in floor["metrics"].items():
-                threshold: dict = metrics["thresholds"][config.metrics_thresholds[0]]
-                precision_cls[classname].append(threshold["precision"])
-                precision.append(threshold["precision"])
-                recall_cls[classname].append(threshold["recall"])
-                recall.append(threshold["recall"])
-                f1_cls[classname].append(threshold["f1"])
-                f1.append(threshold["f1"])
-        iou: float = np.mean(iou)
-        iou_cls: dict = {key: np.mean(value) for key, value in iou_cls.items()}
-        precision: float = np.mean(precision)
-        precision_cls: dict = {key: np.mean(value) for key, value in precision_cls.items()}
-        recall: float = np.mean(recall)
-        recall_cls: dict = {key: np.mean(value) for key, value in recall_cls.items()}
-        f1: float = np.mean(f1)
-        f1_cls: dict = {key: np.mean(value) for key, value in f1_cls.items()}
+                for threshold, values in metrics["thresholds"].items():
+                    for name, value in values.items():
+                        if name == "matched":
+                            continue
+                        # Collect average over classes metrics
+                        global_metrics_output[name][threshold]["average"].append(value)
+                        metrics_output[name][threshold]["average"].append(value)
+                        # Collect classes specific metrics
+                        global_metrics_output[name][threshold][classname].append(value)
+                        metrics_output[name][threshold][classname].append(value)
 
-        data["iou"]["global"] = iou
-        data["iou"]["classes"] = iou_cls
-        data["precision"]["global"] = precision
-        data["precision"]["classes"] = precision_cls
-        data["recall"]["global"] = recall
-        data["recall"]["classes"] = recall_cls
-        data["f1"]["global"] = f1
-        data["f1"]["classes"] = f1_cls
+        data = {}
+        for name, thresholds in metrics_output.items():
+            data[name] = {}
+            for threshold, classnames in thresholds.items():
+                data[name][threshold] = {}
+                for classname, values in classnames.items():
+                    data[name][threshold][classname] = np.mean(values)
+        data["iou"] = {}
+        for classname, values in iou_output.items():
+             data["iou"][classname] = np.mean(values)
         data["floors"] = floors
 
         logging.info("Matching data export...")
         with open(output.joinpath(f"{model}.json"), "w+", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4, cls=utils.NumpyArrayEncoder)
 
-        for key, value in precision_cls.items():
-            global_metrics[f"precision_{key}"].append(value)
-        global_metrics["precision"].append(precision)
-        for key, value in recall_cls.items():
-            global_metrics[f"recall_{key}"].append(value)
-        global_metrics["recall"].append(recall)
-        for key, value in f1_cls.items():
-            global_metrics[f"f1_{key}"].append(value)
-        global_metrics["f1"].append(f1)
-        for key, value in iou_cls.items():
-            global_metrics[f"iou_{key}"].append(value)
-        global_metrics["iou"].append(iou)
-
     with open(output.joinpath(f"scores.txt"), "w+") as file:
         file.write("submitted: 1\n")
-        for key, values in global_metrics.items():
-            file.write(f"{key}: {np.mean(values):.4f}\n")
+        for name, thresholds in global_metrics_output.items():
+            for threshold, classnames in thresholds.items():
+                for classname, values in classnames.items():
+                    file.write(f"{threshold * 100:.0f}cm_{name}_{classname}: {np.mean(values):.2f}\n")
+        for classname, values in global_iou_output.items():
+            file.write(f"{classname}_IoU: {np.mean(values):.2f}\n")
         file.close()
 
 if __name__ == "__main__":
